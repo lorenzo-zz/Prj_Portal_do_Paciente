46f4c7b3efd8f89b438b8e1b3061b5372f2fe08e
Pedro Henrique de Borba <PedroHenriquede.Borba@philips.com>
2025-05-07 12:59:40 -0300

    Includes(phhborba) Subindo Spring Correto

---
+++ Back_End/src/test/java/br/com/philips/patiente_portal/PatientePortalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.philips.patiente_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientePortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
