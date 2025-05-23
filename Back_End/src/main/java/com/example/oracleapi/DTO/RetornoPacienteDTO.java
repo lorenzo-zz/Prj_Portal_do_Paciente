3f0815de2fb27d77a523fcc4321a19d07eff1268
Gabriel Eduardo Ritter <gabrieleduardo.ritter@philips.com>
2025-05-23 11:46:21 -0300

    novas coisas

---
+++ Back_End/src/main/java/com/example/oracleapi/DTO/RetornoPacienteDTO.java
@@ -0,0 +1,20 @@
+package com.example.oracleapi.DTO;
+
+import java.time.LocalDate;
+
+public record RetornoPacienteDTO(
+            String nome,
+            String telefone,
+            String email,
+            LocalDate dataNascimento,
+            String cep,
+            String logradouro,
+            String numero,
+            String complemento,
+            String bairro,
+            String cidade,
+            String uf
+
+) {
+    
+}
